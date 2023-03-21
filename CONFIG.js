14d4789c22ee85f19ee4a9a4fd751e9d327a3082
djtmema <128503267+djtmema@users.noreply.github.com>
2023-03-21 23:10:19 +0700

    Initial commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "tặng skin em nhé, i love you",
+  descriptionHeader:
+    "Nếu ae thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: "Cảm ơn ae vì đồng ý",
+  descriptionModar:
+    'em biết là ae sẽ đồng ý tặng skin em mà ^^ </br> Cám ơn ae đã đồng ý để cho em đc nhận skin của ae.Đối với em, ae mãi fa :)) </br> "chúc ae mãi fa. </br>  chúc ae mãi fa "',
+};
